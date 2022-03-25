dde72ce86abffb5586b707c3e1742c68b3f28902
Sudheer <rskumar780@gmail.com>
2022-03-25 11:32:38 +0530

    Added SpringTest

---
+++ src/test/java/com/example/demo/SpringTest123.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class SpringTest123 {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
